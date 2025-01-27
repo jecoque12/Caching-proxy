a29732c4a5742154bfd2138ac6583004de59c263
jecoque12 <jonathan.coque12@gmail.com>
2025-01-27 23:23:48 +0100

    first commit

---
+++ Options.cs
@@ -0,0 +1,10 @@
+﻿namespace caching_proxy
+{
+    public class Options
+    {
+        public const string Port = "--port";
+        public const string Origin = "--origin";
+        public const string abbreviationPort = "-p";
+        public const string abbreviationOrigin = "-o";
+    }
+}
